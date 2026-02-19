3187977f15ea28870d8e4fc5340bcde0302035d9
ShadowWarrior23 <127506020+ShadowWarrior23@users.noreply.github.com>
2026-02-19 07:08:27 +0100

    f1

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import CalcComp from './components/CalcComp';
 function App() {
 
   return (
+    <header>
+      
+    </header>
     <section className='h-screen flex items-center justify-center'>
       <CalcComp />
     </section>
